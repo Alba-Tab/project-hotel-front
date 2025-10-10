36ce28fdcc28e5e9c9ad1be19ba2f9345b0dc4dd
brayan <brayansamsungfame@gmail.com>
2025-10-10 02:05:05 -0400

    vista de formulario hecha pero falta que funcione

--- src/app/pages/hoteles/crear-hotel/crear-hotel.component.ts
+++ src/app/pages/hoteles/crear-hotel/crear-hotel.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,9 +8,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
+import { CommonModule } from '@angular/common';
+import { Value } from 'sass';
+import { HotelService } from 'src/app/services/hotel.service';
+import { Router } from '@angular/router';
+import { Hotel } from 'src/app/interfaces/hotel.interface';
 
-interface Food {
-  value: string;
+interface Estado {
+  value: boolean;
   viewValue: string;
 }
 
@@ -18,6 +23,10 @@ interface Food {
 @Component({
   selector: 'app-crear-hotel',
   imports: [
+
+    CommonModule,
+    FormsModule,
+
     MatFormFieldModule,
     MatSelectModule,
     FormsModule,
@@ -33,10 +42,48 @@ interface Food {
 })
 export class CrearHotelComponent {
 
-  state: Food[] = [
-    { value: 'steak-0', viewValue: 'Activo' },
-    { value: 'pizza-1', viewValue: 'Inactivo' },
-  ];
+  hotelForm: FormGroup;
 
-  selectedState = this.state[1].value;
+  estados: Estado[] = [
+    { value: true, viewValue: 'Activo' },
+    { value: false, viewValue: 'Inactivo' },
+  ]
+
+  categorias = [1,2,3,4,5];
+
+  constructor(
+    private fb: FormBuilder,
+    private hotelService: HotelService,
+    private router: Router
+  ) {
+    this.hotelForm = this.fb.group({
+      nombre: ['', [Validators.required]],
+      direccion: ['', [Validators.required]],
+      telefono: ['', [Validators.required]],
+      categoria: [4, [Validators.required, Validators.min(1), Validators.max(5)]],
+      estado: [true, [Validators.required]],
+    })
+  }
+
+  onSubmit() {
+    if (this.hotelForm.valid) {
+      const hotel: Hotel = this.hotelForm.value;
+
+      this.hotelService.createHotel(hotel).subscribe({
+        next: (response) => {
+          console.log('hotel creado');
+          this.router.navigate(['/hoteles']);
+        },
+        error: (error) => {
+          console.log('Error al crear hotel: ', error);
+        }
+      });
+    } else {
+      console.log('Formulario invalido');
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['/hoteles']);
+  }
 }
