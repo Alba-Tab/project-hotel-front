b6368ca0c911e537d1cdfab5cd262515a3f34c2f
brayan <brayansamsungfame@gmail.com>
2025-10-13 19:22:52 -0400

    para q jairo deje de llorar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   // apiUrl: 'http://localhost:3000/api'
   // apiUrl: 'http://hotelsol.localhost:8000/api'
   // apiUrl: 'http://test123.localhost:8000/api'
-  apiUrl: 'http://hotelsol.localhost:8000/api'
+  // apiUrl: 'http://hotelsol.localhost:8000/api'
   // apiUrl: 'http://test123.localhost:8000/api'
   // apiUrl: 'http://localhost:8000/api',
 };
